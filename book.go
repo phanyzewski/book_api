2e4f1b9f958f6587989f920c33d16437aa76bbda
Paul Hanyzewski <phanyzewski@enova.com>
2018-04-14 12:21:36 -0600

    wire up db hooks

--- book.go
+++ book.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"database/sql"
-	"errors"
-)
-
-type book struct {
+type Book struct {
 	ID            string `json:"id,omitempty"`
 	Title         string `json:"title,omitempty"`
 	PublishedDate string `json:"publishedDate,omitempty"`
@@ -15,22 +10,42 @@ type book struct {
 	// Author        *Author       `json:"author,omitempty"`
 }
 
-func (p *book) getBook(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (b *Book) getBook(db *sqlx.DB) error {
+	book := Book{}
+	err := db.Get(&book, "SELECT title, FROM books WHERE id=$1", b.ID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return book, nil
 }
 
-func (p *book) updateBook(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (b *Book) updateBook(db *sqlx.DB) error {
+	_, err := db.MustExec("UPDATE books set title=$1 WHERE id=$2", b.Title, b.ID)
+
+	return err
 }
 
-func (p *book) deleteBook(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (b *Book) deleteBook(db *sqlx.DB) error {
+	_, err := db.MustExec("DELETE FROM books WHERE id=$1", b.Title, b.ID)
+
+	return err
 }
 
-func (p *book) createBook(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (b *Book) createBook(db *sqlx.DB) error {
+	_, err := db.MustExec("INSERT INTO books(title) VALUES($1) RETURNING id ", b.Title)
+
+	return err
 }
 
-func getBooks(db *sql.DB, start, count int) ([]book, error) {
-	return nil, errors.New("Not implemented")
+func getBooks(db *sqlx.DB, start, count int) ([]Book, error) {
+	books := []Book{}
+	err := db.Select(&books, "SELECT id, title FROM books")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return books, nil
 }
