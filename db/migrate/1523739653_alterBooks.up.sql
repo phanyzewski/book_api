98ca41118ed48db70b093fb74e4e0b4b25a5cd6f
Paul Hanyzewski <phanyzewski@enova.com>
2018-04-15 11:50:23 -0600

    add ratings and status to books

--- db/migrate/1523739653_alterBooks.up.sql
+++ db/migrate/1523739653_alterBooks.up.sql
@@ -1,3 +1,3 @@
-ALTER TABLE books
+ALTER TABLE IF EXISTS books
 ADD COLUMN author_id integer,
 ADD FOREIGN KEY (author_id) REFERENCES authors(id);
--- db/migrate/1523753980_AddPublisherReference.up.sql
+++ db/migrate/1523753980_AddPublisherReference.up.sql
@@ -1,4 +1,4 @@
 
-ALTER TABLE books
+ALTER TABLE IF EXISTS books
 ADD COLUMN publisher_id integer,
 ADD FOREIGN KEY (publisher_id) REFERENCES publishers(id);
